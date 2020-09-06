427f968d01e805f504986e1aee355f32cf179a45
Dixit <Shashank.Dixit@philips.com>
2020-09-06 23:14:10 +0530

    added Stats.cs class

---
+++ Statistics/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics
+{
+    public class Stats
+    {
+        public double min;
+        public double max;
+        public double average;
+    }
+}
